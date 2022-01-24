f46900e9bc6f5c133b7e49610468f7fe148f14e7
Yi-Ting <abc10512106@gmail.com>
2022-01-24 15:03:10 +0800

    建立provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
-  return <div className="App">這裡是首頁</div>;
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BookList() {
+  return (
+    <div className="book-list">
+      <ul>
+        <li>麥田捕手</li>
+        <li>原子習慣</li>
+        <li>如何利用優勢創業</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
